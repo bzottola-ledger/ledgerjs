b0cf51a866d21878588de75ad105317b5cb86de5
bzottola-ledger <98770457+bzottola-ledger@users.noreply.github.com>
2022-08-15 15:36:53 -0700

    Update trc20.js

--- packages/cryptoassets/data/trc20.js
+++ packages/cryptoassets/data/trc20.js
@@ -6,5 +6,6 @@ module.exports = [
   ["TVj7RNVHy6thbM7BWdSe9G6gXwKhjhdNZS","KLV","Klever","TVj7RNVHy6thbM7BWdSe9G6gXwKhjhdNZS",6],
   ["TBwoSTyywvLrgjSgaatxrBhxt3DGpVuENh","SEED","SEED","TBwoSTyywvLrgjSgaatxrBhxt3DGpVuENh",6],
   ["TZJmk51TP3YEETw7phRP581A7H3VzLDFsa","TEUR","EUR AlteWay Token","TZJmk51TP3YEETw7phRP581A7H3VzLDFsa",4],
-  ["TAFjULxiVgT4qWk6UZwjqwZXTSaGaqnVp4","BTT","BitTorrent","TAFjULxiVgT4qWk6UZwjqwZXTSaGaqnVp4",18]  
+  ["TAFjULxiVgT4qWk6UZwjqwZXTSaGaqnVp4","BTT","BitTorrent","TAFjULxiVgT4qWk6UZwjqwZXTSaGaqnVp4",18],  
+  ["TUpMhErZL2fhh4sVNULAbNKLokS4GjC1F4","TUSD","TrueUSD","TUpMhErZL2fhh4sVNULAbNKLokS4GjC1F4",18]
 ];
